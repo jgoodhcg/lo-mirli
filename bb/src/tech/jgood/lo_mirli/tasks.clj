2977c1c2fd5daf08cdbd82ea3b8e43af17d1a29f
Justin <jgoodhcg@gmail.com>
2023-10-07 14:36:20 -0400

    First commit

---
+++ bb/src/tech/jgood/lo_mirli/tasks.clj
@@ -0,0 +1,6 @@
+(ns tech.jgood.lo-mirli.tasks)
+
+(defn hello
+  "An example of a custom task."
+  []
+  (println "Hello there."))
---
+++ src/tech/jgood/lo_mirli/settings.clj
@@ -0,0 +1,3 @@
+(ns tech.jgood.lo-mirli.settings)
+
+(def app-name "My Application")
